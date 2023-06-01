0992c4b4c329dedb4965d7e028447ac242627bd1
Sikarev <roman.sikarev@kepler.team>
2023-06-01 17:47:41 +0300

    Added 3D PC model

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-npm i --leagacy-peer-deps @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom
+npm i --leagacy-peer-deps @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom three
 
 react-tilt
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { styles } from '../styles.js';
+import { ComputersCanvas } from './canvas/index.js';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm&nbsp;
+            <span className="text-[#915eff]">Roman</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-w text-white-100`}>
+            I develop user <br className="sm:block hidden"/>interfaces and web applications
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas/>
+    </section>
   )
 }
 
